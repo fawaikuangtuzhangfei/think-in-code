e5db732aa7306653c99ab8e8714bd03049936cb9
Java男朋友 <775159840@qq.com>
2023-06-29 07:06:56 +0800

    feat:遍历树-二色遍历法

--- think-in-leetcode/src/main/java/com/think/leetcode/editor/cn/entity/TreeNode.java
+++ think-in-leetcode/src/main/java/com/think/leetcode/editor/cn/entity/TreeNode.java
@@ -2,8 +2,6 @@ package com.think.leetcode.editor.cn.entity;
 
 import com.think.leetcode.editor.cn.ZhongJianErChaShuLcof;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Stack;
 
 /**
@@ -16,26 +14,31 @@ public class TreeNode {
     public TreeNode right;
     public TreeNode(int x) { val = x; }
 
+    /**
+     * 二色标记法:
+     * 白节点=未访问，灰节点=已访问，不断弹栈
+     * 1.遇到白节点，置为灰，将其左右子树压栈
+     * 2.遇到灰节点，打印
+     * @param root  根节点
+     */
     public static void front(TreeNode root){
         Stack<Object> stack = new Stack<>();
-        List<Integer> list = new ArrayList<>();
         stack.push(root);
         while(!stack.empty()){
             Object pop = stack.pop();
             if(pop instanceof Integer){
                 // 已访问
-
+                System.out.println(pop);
             }else{
                 // 未访问
                 TreeNode node = (TreeNode)pop;
-                System.out.println(node.val);
+                if(node.right != null){
+                    stack.push(node.right);
+                }
+                if(node.left != null){
+                    stack.push(node.left);
+                }
                 stack.push(node.val);
-                if(node.right != null){
-                    stack.push(node.right);
-                }
-                if(node.right != null){
-                    stack.push(node.right);
-                }
             }
         }
 
